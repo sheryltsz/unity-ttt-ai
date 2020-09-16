b44f422588c9ef4d37303aa7b869500bd99ee0c3
sheryltsz <70138510+sheryltsz@users.noreply.github.com>
2020-09-16 15:16:12 +0800

    Initialise basic ttt game files
    Added the basic ttt game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+
+    private int moveCount;
+    public GameObject restartButton;
+
+    void Awake() 
+    {
+        gameOverPanel.SetActive(false);
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+        moveCount = 0;
+        restartButton.SetActive(false);
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++) 
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTime()
+    {
+        moveCount++;
+        //game is small enough to check if win using bruteforce
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+        }
+        else if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+        }
+        else
+        {
+            if (moveCount >= 9) {
+                gameOverText.text =" It's a draw!";
+                gameOverPanel.SetActive(true);
+                restartButton.SetActive(true);
+
+            }
+            ChangeSides();
+        }
+    }
+
+    void GameOver()
+    {
+        for(int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;        
+        }
+
+        gameOverText.text = playerSide + " wins!";
+        gameOverPanel.SetActive(true);
+        restartButton.SetActive(true);
+
+    }
+
+    void ChangeSides() 
+    {
+        if (playerSide == "X") { playerSide = "O"; }
+        else { playerSide = "X"; }
+    }
+
+    public void RestartGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = true;
+            buttonList[i].text = "";
+        }
+    }
+}
---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+
+    private GameController gameController;
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTime();
+    }
+
+    public void SetGameControllerReference(GameController controller) 
+    {
+        gameController = controller;
+    }
+}
